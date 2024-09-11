dc15e226682eefc000a60c9333ead890ad8fe45b
kapilbamotriya123 <kapilbamotriya12345@gmail.com>
2024-09-11 06:58:37 +0530

    fixed minor bugs

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -6,8 +6,11 @@ const Navbar = () => {
   return (
     <header className="sticky top-0 z-10 bg-card shadow-sm">
       <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-center gap-5 px-5 py-3">
-        <Link href="/" className="text-green-500- text-2xl font-bold">
+        <Link href="/" className="hidden sm:inline text-green-500- text-xl sm:text-2xl font-bold">
           Kapil.
+        </Link>
+        <Link href="/" className="sm:hidden text-green-500- text-xl sm:text-2xl font-bold">
+          K
         </Link>
         <SearchField />
         <UserButton className={'sm:ms-auto'} />
