9274e21efbce78989233589aecf952f408819871
kapilbamotriya123 <kapilbamotriya12345@gmail.com>
2024-09-11 06:15:06 +0530

    minor changes

--- src/app/(main)/MenuBar.tsx
+++ src/app/(main)/MenuBar.tsx
@@ -4,12 +4,14 @@ import { Bell, Bookmark, Home, Mail } from 'lucide-react';
 import NotificationButton from './NotificationButton';
 import { validateRequest } from '@/auth';
 import prisma from '@/lib/prisma';
+import UserAvatar from '@/components/UserAvatar';
 
 interface MenuBarProps {
    className?: string;
+   userAvatar?: string | null
 }
 
-const MenuBar = async ({ className }: MenuBarProps) => {
+const MenuBar = async ({ className, userAvatar }: MenuBarProps) => {
    const { user } = await validateRequest();
 
    if (!user) return null;
@@ -36,17 +38,7 @@ const MenuBar = async ({ className }: MenuBarProps) => {
             </Link>
          </Button>
          <NotificationButton initialState={{unreadCount}} />
-         <Button
-            variant="ghost"
-            className="flex items-center justify-start gap-3"
-            title="Messages"
-            asChild
-         >
-            <Link href="/messages">
-               <Mail />
-               <span className="hidden lg:inline">Messages</span>
-            </Link>
-         </Button>
+
          <Button
             variant="ghost"
             className="flex items-center justify-start gap-3"
@@ -56,6 +48,17 @@ const MenuBar = async ({ className }: MenuBarProps) => {
             <Link href="/bookmarks">
                <Bookmark />
                <span className="hidden lg:inline">BookMarks</span>
+            </Link>
+         </Button>
+         <Button
+            variant="ghost"
+            className="flex items-center justify-start gap-3"
+            title="profile"
+            asChild
+         >
+            <Link href="/messages">
+               <UserAvatar avatarUrl={userAvatar}  size={30}/>
+               <span className="hidden lg:inline">Profile</span>
             </Link>
          </Button>
       </div>
