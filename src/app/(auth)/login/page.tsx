3e90e39c818aee3ffc1681bbc5909fd721f6b60a
kapilbamotriya123 <kapilbamotriya12345@gmail.com>
2024-09-14 06:33:59 +0530

    updated the read me file

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,9 @@
 import { Metadata } from "next";
 import Image from "next/image";
-import loginImage from '@/assets/iitd.webp'
+import loginImage from '@/assets/login-image.jpg'
 import LoginForm from "./LoginForm";
 import Link from "next/link";
-// import GoogleSignInButton from "../../../lib/google/GoogleSignInButton";
+import GoogleSignInButton from "./google/GoogleSignInButton";
 
 export const metadata: Metadata = {
   title: 'Login'
@@ -15,15 +15,15 @@ const LoginPage = () => {
         <div className='flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl bg-card shadow-2xl'>
           <div className='w-full space-y-10 overflow-y-auto p-10 md:w-1/2'>
             <h1 className={'text-3xl font-bold text-center'}>Login to Kapil&#39;s app</h1>
-            {/* <div className={'space-y-5'}> */}
-              {/* <GoogleSignInButton /> */}
-              {/* <div className="flex items-center gap-3">
+            <div className={'space-y-5'}>
+              <GoogleSignInButton />
+              <div className="flex items-center gap-3">
                 <div className="h-px bg-muted flex-1" />
                 <span>OR</span>
                 <div className="h-px bg-muted flex-1" />
-              </div> */}
+              </div>
               <LoginForm />
-            {/* </div> */}
+            </div>
             <Link href='/signup' className='block text-center hover:underline'>Don&#39;t have an account? Sign up</Link>
           </div>
           <Image src={loginImage} alt='login page image' className='hidden w-1/2 md:block object-cover '/>
